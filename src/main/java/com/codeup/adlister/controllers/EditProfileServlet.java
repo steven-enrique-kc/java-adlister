d50ae6ca83a37830e85f607021ba8ea002317e79
Steven Quintanilla <steven.quintanilla1907@gmail.com>
2018-12-18 15:23:58 -0600

    finished update profile feature

---
+++ src/main/java/com/codeup/adlister/controllers/EditProfileServlet.java
@@ -0,0 +1,74 @@
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "controllers.EditProfileServlet", urlPatterns = "/editprofile")
+public class EditProfileServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String passwordConfirmation = request.getParameter("confirm_password");
+
+        // validate input
+        boolean duplicateUsername = false;
+        if (DaoFactory.getUsersDao().findByUsername(username) != null){
+            duplicateUsername = true;
+        }
+
+        boolean inputHasErrors = username.isEmpty()
+                || email.isEmpty()
+                || password.isEmpty()
+                || (! password.equals(passwordConfirmation));
+
+        if (inputHasErrors) {
+            response.sendRedirect("/editprofile");
+            return;
+        }
+
+        if (duplicateUsername){
+            request.setAttribute("hasDuplicate", true);
+            request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+//            response.sendRedirect("/register");
+            return;
+        }
+
+        // edit the user and set attributes
+        request.removeAttribute("hasDuplicate");
+        User user = DaoFactory.getUsersDao().findByUsername((String) request.getSession().getAttribute("username"));
+        user.setEmail(email);
+        user.setUsername(username);
+        user.setPassword(password);
+
+
+        DaoFactory.getUsersDao().editUser(user);
+        request.getSession().setAttribute("user", user);
+        List<Ad> userAds = DaoFactory.getAdsDao().getUsersAds(user.getId());
+
+        request.getSession().setAttribute("username", user.getUsername());
+        request.getSession().setAttribute("password", user.getPassword());
+        request.getSession().setAttribute("email", user.getEmail());
+        request.getSession().setAttribute("userid", user.getId());
+        response.sendRedirect("/profile");
+    }
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/editprofile");
+            return;
+        }
+
+        request.getRequestDispatcher("/WEB-INF/editprofile.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/WEB-INF/editprofile.jsp
@@ -0,0 +1,37 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <jsp:include page="partials/head.jsp">
+        <jsp:param name="title" value="Edit Your Profile" />
+    </jsp:include>
+</head>
+<body>
+<jsp:include page="partials/navbar.jsp" />
+<div class="container">
+    <h1>Edit Your Profile Information</h1>
+    <c:if test="${hasDuplicate != null}">
+        <p style="color: red">Sorry, Username Taken</p>
+    </c:if>
+    <form action="/editprofile" method="post">
+        <div class="form-group">
+            <label for="username">Username</label>
+            <input id="username" name="username" class="form-control" type="text">
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input id="email" name="email" class="form-control" type="text">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input id="password" name="password" class="form-control" type="password">
+        </div>
+        <div class="form-group">
+            <label for="confirm_password">Confirm Password</label>
+            <input id="confirm_password" name="confirm_password" class="form-control" type="password">
+        </div>
+        <input type="submit" class="btn btn-primary btn-block">
+    </form>
+</div>
+</body>
+</html>
